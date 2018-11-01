26592f0a92b787c2ab4666df7ad2069f3662e7e3
Nicholas La Branche <nicholas@Nicholass-MacBook-Pro.local>
2018-11-01 13:32:53 -0400

    add dom_node_collection, rename html

---
+++ jQuery_project/lib/dom_node_collection.js
@@ -0,0 +1,7 @@
+// In this phase we are going to create a class to hold DOM nodes and offer
+// convenient methods for traversal and manipulation. We will implement
+// empty, remove, attr, addClass, removeClass, html, find, children, and parent.
+
+
+
+module.exports = DomNodeCollection;
